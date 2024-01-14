4ab65e07536a90034f58439a557195231aef5d57
Urtekin77 <urtekinedanur77@gmail.com>
2024-01-15 01:16:52 +0300

    Tasarım Çalışması 1

---
+++ app/src/main/java/com/example/moviapp/Util/Credentials.java
@@ -0,0 +1,8 @@
+package com.example.moviapp.Util;
+
+public class Credentials {
+    // https://api.themoviedb.org/3/movie/popular?api_key=9634dbf509502457250f916d4a993daf
+
+    public static final String BASE_URL="https://api.themoviedb.org/3/";
+    public static final String API_KEY="9634dbf509502457250f916d4a993daf";
+}
