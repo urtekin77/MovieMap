f780fbe6aaa9d13e18bebe179019483e070187ae
urtekin77 <121871723+urtekin77@users.noreply.github.com>
2024-01-15 13:54:00 +0300

    Credentials.java

--- app/src/main/java/com/example/moviapp/Util/Credentials.java
+++ app/src/main/java/com/example/moviapp/Util/Credentials.java
@@ -1,8 +1,6 @@
 package com.example.moviapp.Util;
 
 public class Credentials {
-    // https://api.themoviedb.org/3/movie/popular?api_key=9634dbf509502457250f916d4a993daf
-
     public static final String BASE_URL="https://api.themoviedb.org/3/";
-    public static final String API_KEY="9634dbf509502457250f916d4a993daf";
+    public static final String API_KEY="YOUR_API_KEY";
 }
